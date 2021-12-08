1929cb0fe5e36401a09cfb8e098ed84d1f94890a
David <davidvignon86@yahoo.fr>
2021-12-08 13:12:07 +0100

    commit

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/AddToFavoriteActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/AddToFavoriteActivity.java
@@ -2,16 +2,12 @@ package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
