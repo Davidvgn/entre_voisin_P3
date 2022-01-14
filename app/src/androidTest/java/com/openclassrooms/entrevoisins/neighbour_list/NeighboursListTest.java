5cab849a92111b3acac3e9cb7da46d56a453917f
David <davidvignon86@yahoo.fr>
2022-01-14 01:39:07 +0100

    Tests instru done

--- app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
@@ -1,50 +1,41 @@
 
 package com.openclassrooms.entrevoisins.neighbour_list;
 
-import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.contrib.RecyclerViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.openclassrooms.entrevoisins.R;
-import com.openclassrooms.entrevoisins.ui.detail.NeighbourDetailsActivity;
 import com.openclassrooms.entrevoisins.ui.neighbour_list.ListNeighbourActivity;
 import com.openclassrooms.entrevoisins.utils.DeleteViewAction;
 
+import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
 import static androidx.test.espresso.action.ViewActions.swipeLeft;
+import static androidx.test.espresso.action.ViewActions.swipeRight;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
-import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isFocused;
-import static androidx.test.espresso.matcher.ViewMatchers.isNotSelected;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static com.openclassrooms.entrevoisins.utils.RecyclerViewItemCountAssertion.withItemCount;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsNull.notNullValue;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
-import android.app.Activity;
-import android.app.Instrumentation;
-
+import android.view.View;
 
 /**
  * Test class for list of neighbours
@@ -60,11 +51,32 @@ public class NeighboursListTest {
     @Rule
     public ActivityTestRule<ListNeighbourActivity> mActivityRule = new ActivityTestRule(ListNeighbourActivity.class);
 
+    //Fragments needs time to load data
+    public static ViewAction waitFor(long delay) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isRoot();
+            }
+
+            @Override
+            public String getDescription() {
+                return "wait for " + delay + "milliseconds";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                uiController.loopMainThreadForAtLeast(delay);
+            }
+        };
+    }
+
     @Before
     public void setUp() {
         mActivity = mActivityRule.getActivity();
         assertThat(mActivity, notNullValue());
     }
+
 
     /**
      * We ensure that our recyclerview is displaying at least on item
@@ -76,6 +88,7 @@ public class NeighboursListTest {
                 .check(matches(hasMinimumChildCount(1)));
 
     }
+
 
     /**
      * When we delete an item, the item is no more shown
@@ -92,10 +105,12 @@ public class NeighboursListTest {
     }
 
     @Test
+    //withText set on 'Chloé'; if myNeighboursList_deleteAction_shouldRemoveItem test is not used,
+    // set text on 'Jack'
     public void launch_NeighbourDetailsActivity() {
         onView(allOf(withId(R.id.list_neighbours), isDisplayed()))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
-        onView(allOf(withId(R.id.neighbour_detail_tv_name), isDisplayed())).check(matches(withText("Jack")));
+        onView(allOf(withId(R.id.neighbour_detail_tv_name), isDisplayed())).check(matches(withText("Chloé")));
 
     }
 
@@ -109,7 +124,20 @@ public class NeighboursListTest {
     }
 
     @Test
-    public void only_favorite_in_FavoritesFragment() {
+    public void only_favorite_in_FavoritesFragment() throws InterruptedException {
+
+        //Swipe to FavoritesFragment
+        onView(allOf(withId(R.id.list_neighbours), isDisplayed()))
+                .perform((swipeLeft()));
+
+        onView(isRoot()).perform(waitFor(1000));
+        onView(allOf(withId(R.id.list_neighbours), isDisplayed())).check(withItemCount(0));
+
+        onView(allOf(withId(R.id.list_neighbours), isDisplayed()))
+                .perform((swipeRight()));
+
+        onView(isRoot()).perform(waitFor(1000));
+
         //Click on first Item
         onView(allOf(withId(R.id.list_neighbours), isDisplayed()))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
@@ -126,6 +154,7 @@ public class NeighboursListTest {
         //Click on FloatingActionButton
         onView(allOf(withId(R.id.neighbour_detail_fab_add_favorite)))
                 .perform((click()));
+
         //Click on 'goBack' button
         onView(Matchers.allOf(withContentDescription("Navigate up"), isDisplayed()))
                 .perform(click());
@@ -134,17 +163,26 @@ public class NeighboursListTest {
         onView(allOf(withId(R.id.list_neighbours), isDisplayed()))
                 .perform((swipeLeft()));
 
-        //Check if the Itemlist only show 2 items.
+        onView(isRoot()).perform(waitFor(1000));
+
+        //Check if the Itemlist only show 2 items (number of items put on favorite).
         onView(allOf(withId(R.id.list_neighbours), isDisplayed())).check(withItemCount(ITEMS_COUNT - 10));
 
-        //todo Prob Matches
+        //Check if the First item is the one we made favorite
         onView(allOf(withId(R.id.list_neighbours), isDisplayed()))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
         onView(allOf(withId(R.id.neighbour_detail_tv_name), isDisplayed())).check(matches(withText("Caroline")));
+
+        //Go back button
+        onView(Matchers.allOf(withContentDescription("Navigate up"), isDisplayed()))
+                .perform(click());
+
+        //Check if the Second item is the one we made favorite
         onView(allOf(withId(R.id.list_neighbours), isDisplayed()))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
-        onView(allOf(withId(R.id.neighbour_detail_tv_name), isDisplayed())).check(matches(withText("Jack")));
-
+        //withText set on 'Chloé'; if myNeighboursList_deleteAction_shouldRemoveItem test is not used,
+        // set text on 'Jack'
+        onView(allOf(withId(R.id.neighbour_detail_tv_name), isDisplayed())).check(matches(withText("Chloé")));
 
 
     }
